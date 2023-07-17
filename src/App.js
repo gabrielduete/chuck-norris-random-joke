7e203dd510ae61d3156efb4854bc03694524c2d2
Gabriel <gabrielmonteiroduete@gmail.com>
2023-07-17 20:56:50 -0300

    chore: remove unnecessary codes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import * as React from 'react'
-import Button from '@mui/material/Button'
+import { Autocomplete, TextField, Button } from '@mui/material'
 
 const App = () => {
+  const options = [
+    { label: 'The Godfather', id: 1 },
+    { label: 'Pulp Fiction', id: 2 },
+  ]
+
   return (
     <div>
-      <Button variant="contained">Hello World</Button>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={options}
+        sx={{ width: 400 }}
+        renderInput={(params) => <TextField {...params} label="Dog Name" />}
+      />
+      <Button variant="contained">SEARCH</Button>
     </div>
   )
 }
